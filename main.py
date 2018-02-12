53fe59112d19817075f54ac52cac9d2e08d0c285
crystaldom <dpacoe@crystal.csus.org>
2018-02-12 13:23:09 -0800

    Did a lot of stuff
    Main now has selling and saffron aquiring functionality. In the process of building an auto selling function

--- main.py
+++ main.py
@@ -1,22 +1,93 @@
 import curses
 from curses import wrapper
-#note that you can use the curses.nodelay() to set a timed input on a typed delay thing. 
+#note that you can use the curses.nodelay() to set a timed input on a typed delay thing.
+import time
+import random
 
 # Classes
 
 # Global Variables
-poundsOfSaffron = 0 #Represents the current amount of saffron (limited by the capacity of the farm))
-saffronPerSecond = 0 #Represents the amount of saffron aquired per second
-money = 0 #Represents the player's money
-farmCapacity = 0 #Represents the capacity of the player's farm (limits the amount of saffron)
-farmLevel = 1 #Represents the farm version that the player has
+farmingLoop = True
+poundsOfSaffron = 0;
+saffronPerSecond = 0;
+money = 0;
+moneyPerSecond = 0;
 
-# Functions
+#Farm Variables
+farmCapacity = 30;
+ogfarmCapacity = 30
+farmLevel = 1
+numberOfAcres = None;
 
+#Selling Variables
+lowerSellingRange = 2
+upperSellingRange = 5
+#Merhant Vars
+merchants = 0;
+regionalMerchants = 0;
+farTravelingMerchants = 0;
+reallyFarTravelingMerchants = 0;
+salesPerSecond = 0;
+
+#Slave Vars BTW this is a work in progress
+slaves = 0;
+skilledSlaves = 0;
 # Functions that simplify curses commands
 
+
 def main(stdscr):
+    def refresh():
+        stdscr.refresh();
+
+    def cprint(text, y, x):
+        stdscr.addstr(y, x, text);
+
+    def title(text):
+        stdscr.addstr(0,0,str(text).center(80),curses.A_REVERSE)
+        refresh()
+
+    def statprint():
+        stdscr.addstr(1,0,("Pounds Of Saffron "+str(poundsOfSaffron)+" | Saffron Per Second " + str(salesPerSecond)).center(80),curses.A_REVERSE)
+        stdscr.addstr(2,0,(" Money "+ str(money) + " | Money/Sales Per Second " + str(moneyPerSecond)).center(80),curses.A_REVERSE)
+        stdscr.addstr(3,0, ("Farm Capacity Left: "+ str(farmCapacity)).center(80), curses.A_REVERSE)
+        refresh()
+
+    def noecho():
+        curses.noecho()
+
+    def automaticSaffonFarmingAlgorith:
+        projectedSaffronGain = 
+    def farm():
+        global poundsOfSaffron
+        global farmingLoop
+        global farmCapacity
+        global money
+        while farmingLoop == True:
+            title("Saffron Aquirer")
+            statprint()
+            noecho()
+            curses.echo()
+            stdscr.move(23,0)
+            farmInput = stdscr.getstr(10)
+            if farmInput.lower() == "saffron" and (poundsOfSaffron + 1) <= ogfarmCapacity:
+                poundsOfSaffron = poundsOfSaffron + 1
+                farmCapacity = farmCapacity - 1
+                stdscr.clear()
+            elif farmInput == "cheat1":
+                poundsOfSaffron = poundsOfSaffron + 100
+                stdscr.clear()
+            elif farmInput == "quit":
+                farmingLoop = False
+            elif farmInput.lower() == "sell":
+                amountOfSaffronSelling = poundsOfSaffron
+                sellingPrice = random.randint(lowerSellingRange, upperSellingRange)
+                money = money + (amountOfSaffronSelling * sellingPrice)
+                poundsOfSaffron = poundsOfSaffron - amountOfSaffronSelling
+                stdscr.clear()
+            else:
+                stdscr.clear()
 
 
+    farm()
 
 wrapper(main)
